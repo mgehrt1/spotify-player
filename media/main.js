6ba2231d44ecfd53f1fc30b78b97172b56d66dbb
Matthew Gehrt <matthewagehrt@gmail.com>
2024-06-16 12:20:45 -0500

    loigin, play, and pause all work

--- media/main.js
+++ media/main.js
@@ -1,30 +1,78 @@
-const handlePreviousButtonClick = () => {
-    console.log("Previous Button Clicked");
-};
+const vscode = acquireVsCodeApi();
 
 const handlePlayButtonClick = () => {
-    console.log("Play Button Clicked");
+    vscode.postMessage({
+        command: "play"
+    });
+    const playButton = document.querySelector('.play-button');
+    const pauseButton = document.querySelector('.pause-button');
+    playButton.style.display = 'none';
+    pauseButton.style.display = 'block';
+};
+
+const handlePauseButtonClick = () => {
+    vscode.postMessage({
+        command: "pause"
+    });
+    const pauseButton = document.querySelector('.pause-button');
+    const playButton = document.querySelector('.play-button');
+    pauseButton.style.display = 'none';
+    playButton.style.display = 'block';
+};
+
+const handlePreviousButtonClick = () => {
+    vscode.postMessage({
+        command: "previous"
+    });
 };
 
 const handleNextButtonClick = () => {
-    console.log("Next Button Clicked");
+    vscode.postMessage({
+        command: "next"
+    });
+};
+
+const handleLoginButtonClick = async () => {
+    vscode.postMessage({
+        command: "login"
+    });
 };
 
 
 // When DOM is loaded, attatch all event listeners
 window.addEventListener('DOMContentLoaded', (event) => {
-    const previousButton = document.querySelector('.previous-button');
-    if (previousButton) {
-        previousButton.addEventListener('click', handlePreviousButtonClick);
-    }
-
     const playButton = document.querySelector('.play-button');
-    if (playButton) {
-        playButton.addEventListener('click', handlePlayButtonClick);
-    }
+    playButton.addEventListener('click', handlePlayButtonClick);
+
+    const pauseButton = document.querySelector('.pause-button');
+    pauseButton.addEventListener('click', handlePauseButtonClick);
+
+    const previousButton = document.querySelector('.previous-button');
+    previousButton.addEventListener('click', handlePreviousButtonClick);
 
     const nextButton = document.querySelector('.next-button');
-    if (nextButton) {
-        nextButton.addEventListener('click', handleNextButtonClick);
+    nextButton.addEventListener('click', handleNextButtonClick);
+    
+    const loginButton = document.querySelector('.login-button');
+    loginButton.addEventListener('click', handleLoginButtonClick);
+});
+
+const login = () => {
+    const loginButton = document.querySelector('.login-button');
+    const controllerContainer = document.querySelector('.controller-container');
+    
+    loginButton.style.display = 'none';
+    controllerContainer.style.display = 'flex';
+};
+
+window.addEventListener('message', event => {
+    const message = event.data;
+    
+    switch (message.command) {
+        case 'login':
+            login();
+            break;
+        default:
+            break;
     }
 });
--- src/extension.ts
+++ src/extension.ts
@@ -1,62 +1,241 @@
 import * as vscode from "vscode";
+import * as crypto from "crypto";
+import * as CryptoJS from "crypto-js";
+import express from "express";
+import axios from "axios";
+
+const clientId = ""; // TODO store this somewhere safe
+const redirectUri = "http://localhost:3000/callback";
+let myExtensionContext: vscode.ExtensionContext;
+let momento: vscode.Memento;
+let provider: SpotifyPlayerViewProvider;
 
 export function activate(context: vscode.ExtensionContext) {
-    const provider = createSpotifyPlayerViewProvider(context.extensionUri);
+    myExtensionContext = context;
+    momento = myExtensionContext.globalState;
 
-    context.subscriptions.push(vscode.window.registerWebviewViewProvider(provider.viewType, provider));
+    provider = new SpotifyPlayerViewProvider(context.extensionUri);
+
+    context.subscriptions.push(vscode.window.registerWebviewViewProvider(SpotifyPlayerViewProvider.viewType, provider));
+
+    provider.isLoggedIn();
 }
 
-const createSpotifyPlayerViewProvider = (extensionUri: vscode.Uri): vscode.WebviewViewProvider & { viewType: string; extensionUri: vscode.Uri; view?: vscode.WebviewView } => {
-    const provider = {
-        viewType: "spotify-player",
-        extensionUri,
-        view: undefined as vscode.WebviewView | undefined,
-        resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext<unknown>, token: vscode.CancellationToken): void | Thenable<void> {
-            provider.view = webviewView;
+class SpotifyPlayerViewProvider implements vscode.WebviewViewProvider {
+    public static readonly viewType = "spotify-player";
 
-            webviewView.webview.options = {
-                // Allow scripts in the webview
-                enableScripts: true,
+    private view?: vscode.WebviewView;
 
-                localResourceRoots: [provider.extensionUri],
-            };
+    constructor(private readonly extensionUri: vscode.Uri) {}
 
-            webviewView.webview.html = getHtmlForWebview(webviewView.webview, provider.extensionUri);
+    public resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken) {
+        this.view = webviewView;
+
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this.extensionUri],
+        };
+
+        webviewView.webview.html = this.getWebviewContent(webviewView.webview);
+
+        webviewView.webview.onDidReceiveMessage(
+            (message) => {
+                switch (message.command) {
+                    case "login":
+                        handleLogin();
+                        break;
+                    case "play":
+                        handlePlay();
+                        break;
+                    case "pause":
+                        handlePause();
+                        break;
+                    case "previous":
+                        vscode.window.showInformationMessage("Previous button clicked");
+                        break;
+                    case "next":
+                        vscode.window.showInformationMessage("Next button clicked");
+                        break;
+                    default:
+                        break;
+                }
+            },
+            undefined,
+            undefined
+        );
+    }
+
+    public isLoggedIn() {
+        const access_token = momento.get("access_token");
+        if (this.view && access_token) {
+            this.view.webview.postMessage({ command: "login" });
+        }
+    }
+
+    private getWebviewContent(webview: vscode.Webview) {
+        const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "main.js"));
+        const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "main.css"));
+
+        const nonce = getNonce();
+
+        return `<!DOCTYPE html>
+			<html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+                    <link href="${styleUri}" rel="stylesheet">
+
+                    <title>Spotify Player</title>
+                </head>
+                <body>
+                    <div class="controller-container">
+                        <button class="previous-button">Previous</button>
+                        <button class="play-button">Play</button>
+                        <button class="pause-button">Pause</button>
+                        <button class="next-button">Next</button>
+                    </div>
+                    <button class="login-button">Login</button>
+
+                    <script nonce="${nonce}" src="${scriptUri}"></script>
+                </body>
+			</html>`;
+    }
+}
+
+const getNonce = () => {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+const handleError = (error: any) => {
+    if (axios.isAxiosError(error)) {
+        console.error(`${error.status}: ${error.message}`);
+    } else {
+        console.error("Unknown error");
+    }
+};
+
+const handleLogin = async () => {
+    const codeVerifier = generateRandomString(64);
+    const hashed = await sha256(codeVerifier);
+    const codeChallenge = base64encode(hashed);
+
+    const scope = "user-read-private user-read-email user-modify-playback-state";
+    const authUrl = new URL("https://accounts.spotify.com/authorize");
+
+    momento.update("code_verifier", codeVerifier);
+
+    const params = {
+        response_type: "code",
+        client_id: clientId,
+        scope,
+        code_challenge_method: "S256",
+        code_challenge: codeChallenge,
+        redirect_uri: redirectUri,
+    };
+
+    authUrl.search = new URLSearchParams(params).toString();
+    await vscode.env.openExternal(vscode.Uri.parse(authUrl.toString()));
+
+    const app = express();
+    app.get("/callback", async (req, res) => {
+        const error = req.query.error;
+        if (error) {
+            res.status(409).send(error);
+        }
+
+        const code = req.query.code as string;
+        getToken(code);
+
+        res.status(200).send(`
+            <!DOCTYPE html>
+            <html>
+              <head>
+                <title>Close Tab</title>
+              </head>
+              <body>
+                <script type="text/javascript">
+                  window.close();
+                </script>
+              </body>
+            </html>
+          `);
+
+        // Once authenticated, the server can close
+        server.close();
+    });
+    const server = app.listen(3000, () => console.log("Listening on port 3000!"));
+};
+
+const generateRandomString = (length: number): string => {
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+};
+
+const sha256 = (plain: string): string => {
+    return CryptoJS.SHA256(plain).toString(CryptoJS.enc.Base64url);
+};
+
+const base64encode = (input: string): string => {
+    return input.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+};
+
+const getToken = async (code: string) => {
+    let codeVerifier = momento.get("code_verifier") as string;
+
+    const payload = new URLSearchParams({
+        client_id: clientId,
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
+    });
+
+    const config = {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
         },
     };
 
-    return provider;
+    const res = await axios.post("https://accounts.spotify.com/api/token", payload, config);
+
+    momento.update("access_token", res.data.access_token);
+    provider.isLoggedIn();
 };
 
-const getHtmlForWebview = (webview: vscode.Webview, extensionUri: vscode.Uri) => {
-    // local path
-    const scriptPath = vscode.Uri.joinPath(extensionUri, "media", "main.js");
-    const stylesPath = vscode.Uri.joinPath(extensionUri, "media", "main.css");
+const handlePlay = async () => {
+    const accessToken = momento.get("access_token");
+    const config = {
+        headers: {
+            Authorization: "Bearer " + accessToken,
+        },
+    };
 
-    // to load into webview
-    const scriptUri = webview.asWebviewUri(scriptPath);
-    const stylesUri = webview.asWebviewUri(stylesPath);
+    try {
+        await axios.put("https://api.spotify.com/v1/me/player/play", {}, config);
+    } catch (error) {
+        handleError(error);
+    }
+};
 
-    return `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+const handlePause = async () => {
+    const accessToken = momento.get("access_token");
+    const config = {
+        headers: {
+            Authorization: "Bearer " + accessToken,
+        },
+    };
 
-            <link href="${stylesUri}" rel="stylesheet"/>
-            <script src="${scriptUri}"></script>
-
-            <title>Spotify Player</title>
-        </head>
-        <body>
-            <div id="main">
-                <h1>Hello Matthew</h1>
-                <div class="controller-container">
-                    <button class="previous-button">Previous</button>
-                    <button class="play-button">Play</button>
-                    <button class="next-button">Next</button>
-                </div>
-            </div>
-        </body>
-        </html>`;
+    try {
+        await axios.put("https://api.spotify.com/v1/me/player/pause", {}, config);
+    } catch (error) {
+        handleError(error);
+    }
 };
